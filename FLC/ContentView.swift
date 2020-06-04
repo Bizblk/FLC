055be1b18ee7624d5ce1cf98ba9715327d499613
Bizblk <enemy_counter@mail.ru>
2020-06-04 20:43:45 +0300

    Initial Commit

---
+++ FLC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FLC
+//
+//  Created by Александр on 04.06.2020.
+//  Copyright © 2020 Bizblk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
