0431774ba5a5701c0f5f7a0f961a5850d6e77aa2
Bizblk <enemy_counter@mail.ru>
2020-06-05 18:43:07 +0300

    add new func

--- FLC/LineCell.swift
+++ FLC/LineCell.swift
@@ -12,18 +12,53 @@ var diametr = lines
 
 struct LineCell: View {
     
-    var line = lines[0]
+    var line = lines[3]
+    
     @State var enterTheWeight = ""
-    @State var result = "0"
+    
+    
+    func sum() -> String{
+        
+      
+        let resultt = String(line.MetrPerKg * (Double(enterTheWeight) ?? 0))
+        return resultt
+    }
+    
+    func sumMetr (metr: Int) -> String {
+        
+        let sum = self.sum()
+        let result = String(Int(sum) ?? 1 + metr )
+        print(result)
+        return result
+    }
+    
     
     
     var body: some View {
         
-        HStack{
-            Text(line.name)
-            TextField("enter the weight", text: $enterTheWeight)
-            Text( "\(line.MetrPerKg * Double(enterTheWeight)!)")
+        HStack {
+            HStack {
+                Text(line.name)
+                TextField("enter the weight", text: $enterTheWeight).frame(width: 150)
+                Text(sum())
+                Spacer()
+            }
+            VStack {
+                ForEach(line.metrs, id: \.self, content: {metr in
+                    HStack {
+                        Text(self.sumMetr(metr: metr))
+                       // Text(self.sumMetr(metr: metr))
+                        
+                    }
+                    
+                    
+                })
+            }
         }
+        
+        
+        
+        
     }
 }
 
--- FLC/Model/Model.swift
+++ FLC/Model/Model.swift
@@ -11,28 +11,31 @@ import Foundation
                         
 
 
-enum Metr: Int {
-    case twentyFive = 25
-    case fifty = 50
-    case hundred = 100
-    case hundredFifty = 150
-}
+//enum Metr: Int {
+//    case twentyFive = 25
+//    case fifty = 50
+//    case hundred = 100
+//    case hundredFifty = 150
+//}
 
 
 
 
-struct Line : Identifiable{
+struct Line : Identifiable, Hashable{
     var id = UUID()
     let name: String
     let MetrPerKg: Double
+    let metrs: [Int]
     
     
 }
  var lines = [
-    Line(name: "0.105", MetrPerKg: 96289),
-    Line(name: "0.128", MetrPerKg: 64794),
-    Line(name: "0.148", MetrPerKg: 48465),
-    Line(name: "0.165", MetrPerKg: 38993)
+    Line(name: "0.105", MetrPerKg: 96289, metrs: [50]),
+    Line(name: "0.128", MetrPerKg: 64794, metrs: [50]),
+    Line(name: "0.148", MetrPerKg: 48465, metrs: [50, 100]),
+    Line(name: "0.165", MetrPerKg: 38993, metrs: [50, 100])
+    
+    
 ]
 
 
