2b2f77a74acefecdd5366a3634e5decc75e43a2c
Bizblk <enemy_counter@mail.ru>
2020-06-06 13:34:10 +0300

    rework cell

--- FLC/LineCell.swift
+++ FLC/LineCell.swift
@@ -17,17 +17,10 @@ struct LineCell: View {
     @State var enterTheWeight = ""
     
     
-    func sum() -> String{
+    func countOfSpool (metr: Int) -> String {
         
-      
-        let resultt = String(line.MetrPerKg * (Double(enterTheWeight) ?? 0))
-        return resultt
-    }
-    
-    func sumMetr (metr: Int) -> String {
         
-        let sum = self.sum()
-        let result = String(Int(sum) ?? 1 + metr )
+        let result = String(Int(line.MetrPerKg * (Double(enterTheWeight) ?? 0) / Double(metr)))
         print(result)
         return result
     }
@@ -36,25 +29,44 @@ struct LineCell: View {
     
     var body: some View {
         
-        HStack {
-            HStack {
-                Text(line.name)
-                TextField("enter the weight", text: $enterTheWeight).frame(width: 150)
-                Text(sum())
+        VStack {
+            HStack() {
+                Spacer(minLength: 10)
+                Text("Dia")
+                Spacer(minLength: 160)
+                Text("Metr/Spool")
                 Spacer()
+                Text("Count/Spool")
+                Spacer(minLength: 30)
             }
-            VStack {
-                ForEach(line.metrs, id: \.self, content: {metr in
-                    HStack {
-                        Text(self.sumMetr(metr: metr))
-                       // Text(self.sumMetr(metr: metr))
+            HStack {
+                
+                HStack {
+                    Spacer()
+                    Text(line.name)
+                    TextField("enter the weight", text: $enterTheWeight).frame(width: 140)
+                    Spacer()
+                }
+                VStack {
+                    ForEach(line.metrs, id: \.self, content: {metr in
+                        HStack {
+                            Text(String(metr))
+                                .frame(width: 50.0)
+                            Spacer()
+                            Text(self.countOfSpool(metr: metr))
+                                .multilineTextAlignment(.center)
+                            Spacer(minLength: 20)
+                            
+                            
+                        }
                         
-                    }
-                    
-                    
-                })
+                        
+                    })
+                }
             }
         }
+        
+        
         
         
         
