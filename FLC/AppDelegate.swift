253592b5cd036dcc2cb7b95d0f9f193550ec3544
Biz <enemy_counter@mail.ru>
2020-07-08 21:16:12 +0300

    break

--- FLC/AppDelegate.swift
+++ FLC/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
 import Firebase
 
 @UIApplicationMain
@@ -18,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-        
+        let db = Firestore.firestore()
+        print(db)
         // Override point for customization after application launch.
         return true
     }
