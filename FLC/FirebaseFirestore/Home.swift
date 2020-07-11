d4ea7412a268afb1d90e0fb44229c2ff4fe4f54b
Biz <enemy_counter@mail.ru>
2020-07-11 12:35:09 +0300

    firestore 1

--- FLC/FirebaseFirestore/Home.swift
+++ FLC/FirebaseFirestore/Home.swift
@@ -12,16 +12,18 @@ import Firebase
 struct Home: View {
     
     @ObservedObject var categories = getCategoriesData()
+    @ObservedObject var line = GetData.init(name: .pure)
     
     var body: some View {
         
         VStack{
-            List(categories.datas) {i in
+            List(line.data) {i in
                 HStack {
                     Text(i.id)
                     Text(i.group)
                     Text(i.name)
                     Text(i.price)
+                    Text(i.dia)
 
 
 
@@ -48,7 +50,7 @@ class getCategoriesData: ObservableObject {
     
     init() {
         let db = Firestore.firestore()
-        db.collection("FishingLine").addSnapshotListener { (snap, err) in
+        db.collection("Sportmaxx").addSnapshotListener { (snap, err) in
             if err != nil{
                 print((err?.localizedDescription)!)
                 return
@@ -58,10 +60,45 @@ class getCategoriesData: ObservableObject {
                 let name = i.document.get("name") as! String
                 let price = i.document.get("price") as! String
                 let group = i.document.get("group") as! String
+                let dia = i.document.get("diameter") as! String
                 
-                self.datas.append(category(id: id, name: name, price: price, pic: id, group: group))
+                self.datas.append(category(id: id, name: name, price: price, pic: id, group: group, dia: dia))
 
             }
+        }
+    }
+}
+
+
+class GetData: ObservableObject {
+    @Published var data = [category]()
+    
+    enum fishing: String{
+        case green = "Deep Green"
+        case pure = "Pure Transparent"
+    }
+    
+   
+    
+    
+    init(name: fishing ) {
+        let db = Firestore.firestore()
+        db.collection("Sportmaxx").whereField("name", isEqualTo: name.rawValue)
+            .getDocuments { (querySnapshot, err) in
+                if  err != nil {
+                    print((err?.localizedDescription)!)
+                } else {
+                    for item in querySnapshot!.documents {
+                        let id = item.documentID
+                        let name = item.get("name") as! String
+                        let price = item.get("price") as! String
+                        let group = item.get("group") as! String
+                        let dia = item.get("diameter") as! String
+                        
+                        self.data.append(category(id: id, name: name, price: price, pic: id, group: group, dia: dia))
+
+                    }
+                }
         }
     }
 }
@@ -74,5 +111,6 @@ struct category: Identifiable {
     var price : String
     var pic : String
     var group : String
+    var dia : String
     
 }
